b114d9865097a3afc8803c7018c18805129c7a97
marpo89 <miltonpomares_13@hotmail.com>
2018-09-04 23:01:44 -0600

    Deploy Signature in Contracts

--- Configuration/Configuration.js
+++ Configuration/Configuration.js
@@ -9,7 +9,7 @@ const ConfigPg = {
   //host: 'localhost', // Server hosting the postgres database
   port: 5432, // env var: PGPORT
   max: 10, // max number of clients in the pool
-  idleTimeoutMillis: 30000 // how long a client is allowed to remain idle before being closed
+  idleTimeoutMillis: 900000 // how long a client is allowed to remain idle before being closed
 }
 
 module.exports = ConfigPg;
