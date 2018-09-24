d7c1347dd80a0971d6bc4fa93f87b43a5a748a81
Olonyl Landeros <jose87.landeros@gmail.com>
2018-09-24 16:35:46 -0600

    Adeded military services mutation

---
+++ graphql/schemas/mutations/applicantMilitaryServiceMutation.js
@@ -0,0 +1,52 @@
+import { inputInsertApplicantLanguage } from '../types/operations/insertTypes';
+import { inputUpdateApplicantLanguage } from '../types/operations/updateTypes';
+import { ApplicantLanguageType } from '../types/operations/outputTypes';
+import Db from '../../models/models';
+
+const ApplicantLanguageMutation = {
+	addApplicantLanguage: {
+		type: ApplicantLanguageType,
+		description: 'Add applicant language record to database',
+		args: {
+			applicantLanguage: { type: inputInsertApplicantLanguage }
+		},
+		resolve(source, args) {
+			return Db.models.ApplicantLanguages.create({
+				ApplicationId: args.applicantLanguage.ApplicationId,
+				idLanguage: args.applicantLanguage.idLanguage,
+				writing: args.applicantLanguage.writing,
+				conversation: args.applicantLanguage.conversation
+			});
+		}
+	},
+	updateApplicantLanguage: {
+		type: ApplicantLanguageType,
+		description: 'Update Applicant Language Record Info',
+		args: {
+			applicantLanguage: { type: inputUpdateApplicantLanguage }
+		},
+		resolve(source, args) {
+			return Db.models.ApplicantLanguages
+				.update(
+					{
+						ApplicationId: args.applicantLanguage.ApplicationId,
+						idLanguage: args.applicantLanguage.idLanguage,
+						writing: args.applicantLanguage.writing,
+						conversation: args.applicantLanguage.conversation
+					},
+					{
+						where: {
+							id: args.applicantLanguage.id
+						},
+						returning: true
+					}
+				)
+				.then(function([ rowsUpdate, [ record ] ]) {
+					if (record) return record.dataValues;
+					else return null;
+				});
+		}
+	}
+};
+
+export default ApplicantLanguageMutation;
