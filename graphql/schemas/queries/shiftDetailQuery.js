725b8e4368d58ba78c6780ba8512089bfbcf0a47
Olonyl Horacio Rocha Landeros <jose87.landeros@gmail.com>
2019-01-02 10:37:36 -0600

    Shift Validation By Date and Employee

--- graphql/schemas/queries/shiftDetailQuery.js
+++ graphql/schemas/queries/shiftDetailQuery.js
@@ -1,6 +1,10 @@
 import { GraphQLList, GraphQLString, GraphQLInt } from 'graphql';
 import { ShiftDetailType } from '../types/operations/outputTypes';
 import Db from '../../models/models';
+import Sequelize from 'sequelize';
+import GraphQLDate from 'graphql-date';
+
+const Op = Sequelize.Op;
 
 const ShiftDetailQuery = {
     ShiftDetail: {
@@ -13,7 +17,65 @@ const ShiftDetailQuery = {
         resolve(root, args) {
             return Db.models.ShiftDetail.findAll({ where: args });
         }
+    },
+    ShiftDetailByDate: {
+        type: new GraphQLList(ShiftDetailType),
+        description: 'List Shift Details  records',
+        args: {
+            startDate: { type: GraphQLDate },
+            endDate: { type: GraphQLDate },
+            employeeId: { type: new GraphQLList(GraphQLInt) },
+            startTime: { type: GraphQLString },
+            endTime: { type: GraphQLString },
+            shiftDetailId: { type: GraphQLInt }
+        },
+        resolve(root, args) {
+            return Db.models.ShiftDetail.findAll({
+                where: {
+                    [Op.and]: [
+                        {
+                            [Op.or]: [
+                                {
+                                    startDate: {
+                                        [Op.lte]: args.endDate,
+                                        [Op.gte]: args.startDate
+                                    }
+                                },
+                                {
+                                    endDate: {
+                                        [Op.lte]: args.endDate,
+                                        [Op.gte]: args.startDate
+                                    }
+                                },
+                            ]
+                        },
+                        {
+                            [Op.or]: [
+                                {
+                                    startTime: { [Op.lte]: args.startTime },
+                                    endTime: { [Op.gte]: args.startTime }
+                                },
+                                {
+                                    startTime: { [Op.lte]: args.endTime },
+                                    endTime: { [Op.gte]: args.endTime }
+                                }
+                            ]
+                        }, {
+                            id: { [Op.ne]: args.shiftDetailId }
+                        }]
+
+                },
+                include: [
+                    {
+                        model: Db.models.ShiftDetailEmployees,
+                        where: { EmployeeId: { [Op.in]: args.employeeId } }
+                    }
+                ]
+
+            });
+        }
     }
+
 };
 
 export default ShiftDetailQuery;
