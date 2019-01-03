b355d2a5366f842f2acd4ed01e0342bd46f612a5
marpo89 <miltonpomares_13@hotmail.com>
2019-01-03 16:48:57 -0600

    Work Order

--- graphql/models/models.js
+++ graphql/models/models.js
@@ -211,16 +211,16 @@ Conn.authenticate()
 		console.error('Unable to connect to the database:', err);
 	});
 
-//Conn.sync({ force: false }).then(() => {
-/*make sure you use false here. otherwise the total data 
-	from the impported models will get deleted and new tables will be created*/
-// now we cann do all db operations on customers table.
-//Ex:- lets read all data
-//	console.log('Applications Inside Connection', Applications.findAll);
-//	Applications.findAll().then((applications) => {
-//console.log('Applications are:-', applications);
-//	});
-//	console.log('sync is completed');
-//});
+Conn.sync({ force: false }).then(() => {
+	/*make sure you use false here. otherwise the total data 
+		from the impported models will get deleted and new tables will be created*/
+	// now we cann do all db operations on customers table.
+	//Ex:- lets read all data
+	//	console.log('Applications Inside Connection', Applications.findAll);
+	//	Applications.findAll().then((applications) => {
+	//console.log('Applications are:-', applications);
+	//	});
+	//	console.log('sync is completed');
+});
 
 export default Conn;
