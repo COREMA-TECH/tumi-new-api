0eeb5581889514f4c9bca2bdadc9312316b2f88c
Olonyl Horacio Rocha Landeros <jose87.landeros@gmail.com>
2019-01-24 16:39:10 -0600

    added dummy function

--- graphql/schemas/queries/shiftQuery.js
+++ graphql/schemas/queries/shiftQuery.js
@@ -2,6 +2,8 @@ import { GraphQLList, GraphQLInt, GraphQLBoolean } from 'graphql';
 import { ShiftType } from '../types/operations/outputTypes';
 import { inputShiftQuery } from '../types/operations/insertTypes';
 
+import GraphQLDate from 'graphql-date';
+
 import Db from '../../models/models';
 
 const ShiftQuery = {
@@ -13,6 +15,27 @@ const ShiftQuery = {
         },
         resolve(root, args) {
             return Db.models.Shift.findAll({ where: args.shift });
+        }
+    },
+    printDate: {
+        type: new GraphQLList(GraphQLDate),
+        description: 'Print Date',
+        args: {
+            startDate: { type: GraphQLDate },
+            endDate: { type: GraphQLDate }
+        },
+        resolve(root, args) {
+            //Create dates to be inserted in ShiftDetail
+            var currentDate = new Date(args.startDate);
+            var list = [];
+
+            while (currentDate <= args.endDate) {
+                let newDate = new Date(currentDate)
+                console.log(newDate, newDate.getDay())
+                list.push(newDate)
+                currentDate.setDate(currentDate.getDate() + 1)
+            }
+            return list;
         }
     }
 };
