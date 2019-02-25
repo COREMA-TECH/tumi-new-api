b73703a3aeb0f059df57c4d630bb5e5aff4664de
Gershell <grlopez90@gmail.com>
2019-02-25 09:21:47 -0600

    I9 mutation

---
+++ graphql/schemas/mutations/applicantI9Mutation.js
@@ -0,0 +1,78 @@
+import { inputInsertApplicantW4 } from '../types/operations/insertTypes';
+import { ApplicantW4Type } from '../types/operations/outputTypes';
+import { GraphQLList, GraphQLInt, GraphQLBoolean, GraphQLString } from 'graphql';
+import pdf from 'html-pdf';
+import AWS from 'aws-sdk';
+import fs from 'fs';
+
+import Db from '../../models/models';
+
+const ApplicantI9Mutation = {
+	addApplicantW4: {
+		type: new GraphQLList(ApplicantW4Type),
+		description: 'Add I9 DocumentType to database',
+		args: {
+			html: { type: GraphQLString },
+			ApplicationId: { type: GraphQLInt }
+		},
+		resolve(source, args) {
+
+            //aqui va la logica del PDF
+			var options = {
+				format: 'Letter',
+				font: 'Arial',
+				size: 12,
+				type: "pdf",             // allowed file types: png, jpeg, pdf
+				quality: "75",           // only used for types png & jpeg
+				orientation: 'portrait',
+				zoomFactor: 1,
+				border: {
+					top: '0.98in', // default is 0, units: mm, cm, in, px
+					right: '0.98in',
+					bottom: '0.98in',
+					left: '0.98in'
+				}
+			};
+			var filename = `i9_${args.ApplicationId}`;
+			var srcFile = `./public/${filename}.pdf`;
+
+			// AWS.config.update({
+			// 	accessKeyId: "AKIAJKPVCC36B44OOXJA",
+			// 	secretAccessKey: "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"
+			// });
+
+			// //aqui va la logica de S3
+			// var s3 = new AWS.S3();
+			// var filePath = srcFile;
+
+			// var params = {
+			// 	Bucket: 'imagestumi',
+			// 	Body : fs.createReadStream(filePath),
+			// 	Key : "PDF/" + path.basename(filePath)
+			// };
+
+			// s3.upload(params, function (err, data) {
+			// 	//handle error
+			// 	if (err) {
+			// 		console.log("Error", err);
+			// 	}
+			// 	//success
+			// 	if (data) {
+			// 		console.log("Uploaded in:", data.Location);
+			// 	}
+			// });
+
+			pdf.create(args.html, options).toFile(srcFile, function (err, res) {
+				if (err) return console.log(err);
+			});
+
+			return Db.models.ApplicantI9.create({ fileName: filename, url: srcFile, fileExtension: ".pdf", completed: true, ApplicationId: args.ApplicationId }, { returning: true }).then((output) => {
+				// return output.map((element) => {
+				// 	return element.dataValues;
+				// });
+			});
+		}
+	},
+};
+
+export default ApplicantI9Mutation;
--- graphql/schemas/mutations/index.js
+++ graphql/schemas/mutations/index.js
@@ -14,6 +14,7 @@ import ApplicantHarassmentPolicyMutation from './applicantHarassmentPolicyMutati
 import ApplicantWorkerCompensationMutation from './applicantWorkerCompensationMutation';
 import ApplicantDocumentMutation from './applicantDocumentMutation';
 import ApplicantW4Mutation from './applicantW4Mutation';
+import ApplicantI9Mutation from './applicantI9Mutation';
 import WorkOrderMutation from './workOrderMutation';
 import WorkOrderPositionMutation from './workOrderPositionMutation';
 import ApplicationPhaseMutation from './applicantionPhaseMutation';
@@ -47,6 +48,7 @@ export {
 	ApplicantWorkerCompensationMutation,
 	ApplicantDocumentMutation,
 	ApplicantW4Mutation,
+	ApplicantI9Mutation,
 	WorkOrderMutation,
 	WorkOrderPositionMutation,
 	ApplicationPhaseMutation,
