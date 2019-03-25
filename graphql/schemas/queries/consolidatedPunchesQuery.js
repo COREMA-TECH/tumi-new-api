bc291c153663c4a9d141eec9c077a795ca9b1314
LaurenceM10 <laurenmontenegro10@gmail.com>
2019-03-25 10:40:56 -0600

    Reformat code in consolidated punches report to create csv file

--- graphql/schemas/queries/consolidatedPunchesQuery.js
+++ graphql/schemas/queries/consolidatedPunchesQuery.js
@@ -1,5 +1,4 @@
-import { GraphQLList, GraphQLString, GraphQLInt } from 'graphql';
-import { MarkedEmployeesType, PunchesReportType } from '../types/operations/outputTypes';
+import {GraphQLInt, GraphQLString} from 'graphql';
 import Db from '../../models/models';
 import GraphQLDate from 'graphql-date';
 import moment from 'moment';
@@ -11,24 +10,25 @@ import os from 'os';
 const Op = Sequelize.Op;
 
 const getPunchesEmployeeFilter = (filter) => {
-    var newFilter = {};
+    let newFilter = {};
 
     //Validate if filter object exists
     if (!filter)
         return newFilter;
 
     //Loop trough eacth filter
-    for (var prop in filter) {
+    for (let prop in filter) {
         //Validate if the filter has value
         if (filter[prop])
-            //Exclude startDate and endDate from filters
+        //Exclude startDate and endDate from filters
             if (!["employee", "startDate", "endDate", "idEntity"].join().includes(prop))
-                newFilter = { ...newFilter, [prop]: filter[prop] };
+                newFilter = {...newFilter, [prop]: filter[prop]};
     }
     return newFilter;
-}
+};
+
 const getPunchesMarkerFilter = (filter) => {
-    var newFilter = {};
+    let newFilter = {};
 
     //Validate if filter object exists
     if (!filter)
@@ -38,70 +38,53 @@ const getPunchesMarkerFilter = (filter) => {
         newFilter = {
             ...newFilter,
             [Op.and]: [
-                { markedDate: { [Op.gte]: filter.startDate } },
-                { markedDate: { [Op.lte]: filter.endDate.setUTCHours(23, 59, 59) } }
+                {markedDate: {[Op.gte]: filter.startDate}},
+                {markedDate: {[Op.lte]: filter.endDate.setUTCHours(23, 59, 59)}}
             ]
-        }
+        };
+
     return newFilter;
-}
+};
 
 const getPunchesCompanyFilter = (filter) => {
-    var newFilter = {};
+    let newFilter = {};
 
     //Validate if filter object exists
     if (!filter)
         return newFilter;
 
-    //Loop trough eacth filter
-    for (var prop in filter) {
+    //Loop trough each filter
+    for (let prop in filter) {
         //Validate if the filter has value
         if (filter[prop])
-            //Only filter by idEntity
+        //Only filter by idEntity
             if (prop == "idEntity")
-                newFilter = { ...newFilter, Id: filter[prop] };
+                newFilter = {...newFilter, Id: filter[prop]};
     }
+
     return newFilter;
-}
+};
 
 const PunchesEmployeesQuery = {
-    markedEmployees: {
-        type: new GraphQLList(MarkedEmployeesType),
-        description: 'List employees records',
-        args: {
-            id: {
-                type: GraphQLInt
-            },
-            typeMarkedId: {
-                type: GraphQLInt
-            },
-            markedDate: {
-                type: GraphQLDate
-            },
-            markedTime: {
-                type: GraphQLString
-            },
-        },
-        resolve(root, args) {
-            return Db.models.MarkedEmployees.findAll({ where: args });
-        }
-    },
     punchesConsolidated: {
-        type: new GraphQLString,
+        type: {
+            filename: GraphQLString
+        },
         description: "Get Punches report",
         args: {
-            idEntity: { type: GraphQLInt },
-            Id_Department: { type: GraphQLInt },
-            employee: { type: GraphQLString },
-            startDate: { type: GraphQLDate },
-            endDate: { type: GraphQLDate },
+            idEntity: {type: GraphQLInt},
+            Id_Department: {type: GraphQLInt},
+            employee: {type: GraphQLString},
+            startDate: {type: GraphQLDate},
+            endDate: {type: GraphQLDate},
         },
         resolve(root, args) {
-            var punches = [];
+            let punches = [];
             return Db.models.MarkedEmployees.findAll({
-                where: { ...getPunchesMarkerFilter(args) },
+                where: {...getPunchesMarkerFilter(args)},
                 include: [{
                     model: Db.models.Employees,
-                    where: { ...getPunchesEmployeeFilter(args) },
+                    where: {...getPunchesEmployeeFilter(args)},
                     as: 'Employees',
                     required: true
                 }, {
@@ -114,23 +97,23 @@ const PunchesEmployeesQuery = {
                     }]
                 }, {
                     model: Db.models.BusinessCompany,
-                    where: { ...getPunchesCompanyFilter(args) },
+                    where: {...getPunchesCompanyFilter(args)},
                     required: true
                 }]
             })
                 .then(marks => {
-                    var objPunches = {};
+                    let objPunches = {};
                     marks.map(_mark => {
-                        var { id, entityId, typeMarkedId, markedDate, markedTime, imageMarked, EmployeeId, ShiftId, flag } = _mark.dataValues;
-                        var key = `${entityId}-${EmployeeId}-${ShiftId}-${moment(markedDate).format('YYYYMMDD')}`;
-                        var employee = _mark.dataValues.Employees.dataValues;
-                        var shift = _mark.dataValues.Shift.dataValues;
-                        var position = shift.CatalogPosition.dataValues;
-                        var company = _mark.dataValues.BusinessCompany.dataValues;
+                        let {id, entityId, typeMarkedId, markedDate, markedTime, imageMarked, EmployeeId, ShiftId, flag} = _mark.dataValues;
+                        let key = `${entityId}-${EmployeeId}-${ShiftId}-${moment(markedDate).format('YYYYMMDD')}`;
+                        let employee = _mark.dataValues.Employees.dataValues;
+                        let shift = _mark.dataValues.Shift.dataValues;
+                        let position = shift.CatalogPosition.dataValues;
+                        let company = _mark.dataValues.BusinessCompany.dataValues;
 
                         //Create new punch object if this object doesnt exist into the array of punches
                         if (!objPunches[key]) {
-                            var reportRow = {
+                            let reportRow = {
                                 employeeId: EmployeeId,
                                 name: `${employee.firstName} ${employee.lastName}`,
                                 hourCategory: '01Reg',
@@ -140,39 +123,51 @@ const PunchesEmployeesQuery = {
                                 hotelCode: company.Code,
                                 positionCode: position.Position,
                             }
-                            objPunches = { ...objPunches, [key]: reportRow }
+                            objPunches = {...objPunches, [key]: reportRow}
                         }
                         //Format punche time
-                        var hour = moment.utc(markedTime, 'HH:mm').format('HH:mm');
+                        let hour = moment.utc(markedTime, 'HH:mm').format('HH:mm');
                         //Update marker type hour based on type and hour
                         switch (typeMarkedId) {
                             case 30570: //Clock In
-                                objPunches[key] = { ...objPunches[key], clockIn: hour, imageMarkedIn: imageMarked, flagMarkedIn: flag, idMarkedIn: id };
+                                objPunches[key] = {
+                                    ...objPunches[key],
+                                    clockIn: hour,
+                                    imageMarkedIn: imageMarked,
+                                    flagMarkedIn: flag,
+                                    idMarkedIn: id
+                                };
                                 break;
                             case 30571://Clock Out
-                                objPunches[key] = { ...objPunches[key], clockOut: hour, imageMarkedOut: imageMarked, flagMarkedOut: flag, idMarkedOut: id };
+                                objPunches[key] = {
+                                    ...objPunches[key],
+                                    clockOut: hour,
+                                    imageMarkedOut: imageMarked,
+                                    flagMarkedOut: flag,
+                                    idMarkedOut: id
+                                };
                                 break;
                             case 30572://Break In
-                                objPunches[key] = { ...objPunches[key], lunchIn: hour };
+                                objPunches[key] = {...objPunches[key], lunchIn: hour};
                                 break;
                             case 30573://Break Out
-                                objPunches[key] = { ...objPunches[key], lunchOut: hour };
+                                objPunches[key] = {...objPunches[key], lunchOut: hour};
                                 break;
                         }
-                    })
+                    });
 
                     //Create array of punches based on object structure
-                    var punches = [];
+                    let punches = [];
                     Object.keys(objPunches).map(i => {
-                        var punche = objPunches[i];//Get Punche Object
-                        var startTime = moment.utc(punche.clockIn, 'HH:mm:ss');//Get Start Time
-                        var endTime = moment.utc(punche.clockOut, 'HH:mm:ss');//Get End Time
-                        var duration = moment.duration(endTime.diff(startTime));//Calculate duration between times
-                        var workedTime = ((duration.hours() + (duration.minutes() / 60)) * 1.00).toFixed(2);//Calulate duration in minutes/float
+                        let punche = objPunches[i];//Get Punche Object
+                        let startTime = moment.utc(punche.clockIn, 'HH:mm:ss');//Get Start Time
+                        let endTime = moment.utc(punche.clockOut, 'HH:mm:ss');//Get End Time
+                        let duration = moment.duration(endTime.diff(startTime));//Calculate duration between times
+                        let workedTime = ((duration.hours() + (duration.minutes() / 60)) * 1.00).toFixed(2);//Calulate duration in minutes/float
 
                         punche.hoursWorked = !isNaN(parseFloat(workedTime)) ? parseFloat(workedTime) : 0; //Update worked time
 
-                        var employee = args.employee || '';
+                        let employee = args.employee || '';
 
                         if (punche.name.trim().toUpperCase().includes(employee.trim().toUpperCase()))
                             punches.push(punche);//Return new object
@@ -209,6 +204,7 @@ const PunchesEmployeesQuery = {
 
                     fs.writeFileSync(filename, output.join(os.EOL));
 
+                    console.log("Filename ----> ", filename.toString());
                     return filename; //Return the filename - path
                 })
         }
