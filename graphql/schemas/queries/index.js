068a5fe7d0a81ec76a6a39e1ba505dab77c33434
marpo89 <miltonpomares_13@hotmail.com>
2019-03-28 16:43:40 -0600

    Tareas de Lenguaje en el employee package y tarea No.TUMI-552

--- graphql/schemas/queries/index.js
+++ graphql/schemas/queries/index.js
@@ -25,6 +25,7 @@ import ShiftQuery from './shiftQuery';
 import ShiftDetailsQuery from './shiftDetailQuery';
 import ShiftWorkOrderQuery from './shiftWorkOrderQuery';
 import MarkedEmployeesQuery from './markedEmployeesQuery';
+import MarkedEmployeesConsolidateQuery from './markedEmployeesConsolidateQuery';
 import ApplicationEmployeesQuery from './applicationEmployeesQuery';
 import ContactsQuery from './contactsQuery';
 import RolesQuery from './roleQuery';
@@ -68,6 +69,7 @@ export {
 	ShiftDetailsQuery,
 	ShiftWorkOrderQuery,
 	MarkedEmployeesQuery,
+	MarkedEmployeesConsolidateQuery,
 	ApplicationEmployeesQuery,
 	ContactsQuery,
 	RolesQuery,
