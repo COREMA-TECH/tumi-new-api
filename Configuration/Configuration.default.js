02c9067ca259b537d72ef0b8e182e97799088525
Gershell López <grlopez90@gmail.com>
2019-04-22 10:21:13 -0600

    new connection string

--- Configuration/Configuration.default.js
+++ Configuration/Configuration.default.js
@@ -1,24 +1,46 @@
 import Sequelize from 'sequelize';
 
-const user = 'corema';
-
 const environments = {
-	dev: { port: '5000', database: 'Tumi_Dev', databasePort: 5432, webPORT: 3001, webURL: 'http://ec2-3-16-143-115.us-east-2.compute.amazonaws.com' },
-	prod: { port: '4001', database: 'TUMI', databasePort: 5432, webPORT: 3000, webURL: 'http://ec2-52-14-244-20.us-east-2.compute.amazonaws.com' },
-	cert: { port: '4000', database: 'Tumi_Certi', databasePort: 5432, webPORT: 3002, webURL: 'http://ec2-3-16-143-115.us-east-2.compute.amazonaws.com' }
+	dev: { 
+		user: 'corema',
+		password: 'S0l040.246.',
+		host: 'coremagroup.cb4kqp6rssxe.us-east-2.rds.amazonaws.com',
+		port: '5000', 
+		database: 'Tumi_Dev', 
+		databasePort: 5432, 
+		webPORT: 3001, 
+		webURL: 'http://ec2-3-16-143-115.us-east-2.compute.amazonaws.com' 
+	},
+	prod: { 
+		user: 'tumiusr',
+		password: 'Nicaragu^&jkiu#z',
+		host: 'tumidb.cnzodd8fyfab.us-east-1.rds.amazonaws.com',
+		port: '4000', 
+		database: 'tumidb', 
+		databasePort: 5432, 
+		webPORT: 3000, 
+		webURL: 'http://app.orionsystem1.com' 
+	},
+	cert: { 
+		user: 'corema',
+		password: 'S0l040.246.',
+		host: 'coremagroup.cb4kqp6rssxe.us-east-2.rds.amazonaws.com',
+		port: '4001', 
+		database: 'Tumi_Certi', 
+		databasePort: 5432, 
+		webPORT: 3002, 
+		webURL: 'http://ec2-3-16-143-115.us-east-2.compute.amazonaws.com' 
+	}
 }
 
 /*****MODIFICAR ACA */
 const STR_ENV = 'dev';
 /*********************/
 
-const { port, database, webPORT, webURL, databasePort } = environments[STR_ENV];
+const { user, password, host, port, database, webPORT, webURL, databasePort } = environments[STR_ENV];
 const URLWeb = `${webURL}:${webPORT}`;
 const URLAccept = `${webURL}/home/schedules-accept`;
 const MY_PORT = port;
-
-const password = 'S0l040.246.';
-const host = 'coremagroup.cb4kqp6rssxe.us-east-2.rds.amazonaws.com';
 
 const nodemailer = require('nodemailer');
 let mailParams = {
@@ -42,7 +64,6 @@ const ConfigPg = {
 	max: 10, // max number of clients in the pool
 	idleTimeoutMillis: 900000 // how long a client is allowed to remain idle before being closed
 };
-
 
 const Conn = new Sequelize(database, user, password, {
 	dialect: 'postgres',
