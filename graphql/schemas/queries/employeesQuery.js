3742452bd9e575d25898ef7f0435753900aea146
John Doe <jose87.landeros@gmail.com>
2019-05-28 13:37:41 -0600

    .

--- graphql/schemas/queries/employeesQuery.js
+++ graphql/schemas/queries/employeesQuery.js
@@ -1,4 +1,4 @@
-import { GraphQLList, GraphQLString, GraphQLBoolean, GraphQLInt } from 'graphql';
+import { GraphQLList, GraphQLString, GraphQLBoolean, GraphQLInt, GraphQLNonNull } from 'graphql';
 import { EmployeesType } from '../types/operations/outputTypes';
 import Db from '../../models/models';
 import moment from 'moment';
@@ -32,33 +32,67 @@ const EmployeesQuery = {
         type: new GraphQLList(EmployeesType),
         description: 'List employees records with social security number',
         args: {
-            id: {
-                type: GraphQLInt
-            },
-            isActive: {
-                type: GraphQLBoolean
-            },
             idEntity: {
-                type: GraphQLInt
+                type: new GraphQLNonNull(GraphQLInt)
             }
         },
         resolve(root, args) {
-            return Db.models.Employees.findAll({
-                where: args,
-                include: [
-                    {
-                        model: Db.models.ApplicationEmployees,
-                        required: true,
+
+            return Db.models.Contacts.findAll({ where: { Id_Entity: args.idEntity } }).then(_contacts => {
+                var applicationIds = [];
+                _contacts.map(_record => {
+                    if (_record.dataValues.ApplicationId)
+                        applicationIds.push(_record.dataValues.ApplicationId);
+                })
+                if (applicationIds.length > 0) {
+                    //Get actives applications associated to contacts from a specific company
+                    return Db.models.Employees.findAll({
+                        where: { isActive: true },
                         include: [
                             {
-                                model: Db.models.Applications,
-                                as: "Application",
-                                required: true
+                                model: Db.models.ApplicationEmployees,
+                                required: true,
+                                include: [
+                                    {
+                                        model: Db.models.Applications,
+                                        as: "Application",
+                                        where: { id: { $in: applicationIds }, isActive: true },
+                                        required: true
+                                    }
+                                ]
                             }
                         ]
-                    }
-                ]
-            });
+                    }).then(_employees => {
+                        _employees.map(_record => {
+                            _record.dataValues.idEntity = args.idEntity;
+                        })
+                        return _employees;
+                    })
+                }
+                else {
+                    //Get active applications assosiated to a specific company
+                    return Db.models.Employees.findAll({
+                        where: { isActive: true, idEntity: args.idEntity },
+                        include: [
+                            {
+                                model: Db.models.ApplicationEmployees,
+                                required: true,
+                                include: [
+                                    {
+                                        model: Db.models.Applications,
+                                        as: "Application",
+                                        where: { isActive: true },
+                                        required: true
+                                    }
+                                ]
+                            }
+                        ]
+                    })
+                }
+
+            })
+
+
         }
     },
 
