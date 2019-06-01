b81887a5683f941b088a83b338fd857d648941f9
Jorge Corea <jcorear94@outlook.com>
2019-06-01 09:42:29 -0600

    Created tables based on models

--- graphql/models/models.js
+++ graphql/models/models.js
@@ -351,8 +351,6 @@ CatalogItem.hasMany(Contacts, {
 	as: 'Contacts'
 })
 
-
-
 // Opening recruiter associations
 OpeningRecruiter.belongsTo(Users, { foreignKey: 'recruiterId' });
 OpeningRecruiter.belongsTo(Shift, { foreignKey: 'openingId' });
@@ -382,7 +380,7 @@ Conn.authenticate()
 		console.error('Unable to connect to the database:', err);
 	});
 
-//Conn.sync({ force: false }).then(() => {
+// Conn.sync({ force: false }).then(() => {
 /*make sure you use false here. otherwise the total data
 	from the impported models will get deleted and new tables will be created*/
 // now we cann do all db operations on customers table.
@@ -392,6 +390,6 @@ Conn.authenticate()
 //console.log('Applications are:-', applications);
 //	});
 //	console.log('sync is completed');
-//});
+// });
 
 export default Conn;
