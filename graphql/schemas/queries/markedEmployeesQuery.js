ed548c719651b868f45d8b6437e8dfa097360fa5
Gershell López <grlopez90@gmail.com>
2019-06-02 15:22:20 -0600

    tardiness query, almost done

--- graphql/schemas/queries/markedEmployeesQuery.js
+++ graphql/schemas/queries/markedEmployeesQuery.js
@@ -1,5 +1,5 @@
 import { GraphQLList, GraphQLString, GraphQLInt } from 'graphql';
-import { MarkedEmployeesType, PunchesReportType } from '../types/operations/outputTypes';
+import { MarkedEmployeesType, PunchesReportType, EmployeesType } from '../types/operations/outputTypes';
 import Db from '../../models/models';
 import GraphQLDate from 'graphql-date';
 import moment from 'moment';
@@ -304,16 +304,59 @@ const MarkedEmployeesQuery = {
             return Db.models.Employees.findAll({
                 where: args,
                 include: [{
+                    model: Db.models.ShiftDetailEmployees,
+                    required: true,
+                    include: [{
+                        model: Db.models.ShiftDetail,
+                        required: true,
+                        order: [['startDate', 'DESC']],
+                    }]
+                },{
                     model: Db.models.MarkedEmployees,
                     required: true,
-                    limit: 1,
                     where: {
-                        markedDate: {
-                            $gt: moment(Date.now()).subtract(2, "week").toDate()
-                        }
+                        typeMarkedId: 30570
                     },
                     order: [['markedDate', 'DESC']],
                 }]
+            }).then(result => {
+                const employees = result;
+                let tardinessData = {};
+                employees.map(employee => {
+                    let ObjKey = `${employee.dataValues.firstName} ${employee.dataValues.lastName}`;
+                    let startDate, startTime, markedTime, markedData = {};
+                  
+                    tardinessData[ObjKey] = {
+                        name: `${employee.dataValues.firstName} ${employee.dataValues.lastName}`
+                    };
+
+                    employee.dataValues.ShiftDetailEmployees.map(ShiftDetailEmployee => {
+                        startDate = moment(ShiftDetailEmployee.dataValues.ShiftDetail.dataValues.startDate).format('YYYYMMDD');
+                        startTime = ShiftDetailEmployee.dataValues.ShiftDetail.dataValues.startTime;
+                        markedData = {
+                            ShiftStart: startTime
+                        }
+                        employee.dataValues.MarkedEmployees.map(MarkedEmployee => {
+                            console.log(moment(MarkedEmployee.dataValues.markedDate).format('YYYYMMDD'));
+                            console.log(startDate);
+                            if (moment(MarkedEmployee.dataValues.markedDate).format('YYYYMMDD') === startDate) {
+                                markedTime = MarkedEmployee.dataValues.markedTime;
+                                markedData = {
+                                    ...markedData,
+                                    ClockIn: markedTime
+                                }
+                            }
+                        });
+                    });
+
+                    tardinessData[ObjKey] = {
+                        ... tardinessData[ObjKey],
+                        markedData
+                    }
+                   
+
+                });
+                //console.log(tardinessData);
             });
         }
     }
