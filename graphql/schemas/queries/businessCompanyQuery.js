41c75cfca4d03d44ad4abe86cfb6d81fbedecd4b
Gershell López <grlopez90@gmail.com>
2019-06-24 16:57:12 -0600

    employeesbyproperties query created

--- graphql/schemas/queries/businessCompanyQuery.js
+++ graphql/schemas/queries/businessCompanyQuery.js
@@ -25,6 +25,53 @@ const businessCompanyQuery = {
                 }]
             });
         }
+    },
+    employeesByProperties: {
+        type: new GraphQLList(BusinessCompanyType),
+        description: 'List of employees by properties',
+        args: {
+            Id: { type: GraphQLInt }
+        },
+        resolve(root, args) {
+            return Db.models.BusinessCompany.findAll({
+                include: [{
+                    model: Db.models.Employees,
+                    include: [{
+                        model: Db.models.PositionRate,
+                        as: 'Title'
+                    }]
+                }, {
+                    model: Db.models.CatalogItem,
+                    where: { Id_Catalog: 8 }
+                }]
+            }).then(ret => {
+                let employeesByProperties = [];
+                let BusinessCompanyObj = {};
+                ret.map(BusinessCompany => {
+                    BusinessCompanyObj = {
+                        code: BusinessCompany.dataValues.Code,
+                        name: BusinessCompany.dataValues.Name,
+                        Employees: []
+                    };
+
+                    BusinessCompany.Employees.map(Employee => {
+                        if (Employee.dataValues) {
+                            BusinessCompanyObj.Employees.push({
+                                id: Employee.dataValues.id,
+                                name: Employee.dataValues.firstName + " " + Employee.dataValues.lastName,
+                                position: Employee.dataValues.PositionRate ? Employee.dataValues.PositionRate.dataValues.Position: 'N/A',
+                                los: 0,
+                                phone: Employee.dataValues.mobileNumber
+                            });
+                        }
+                    });
+
+                    employeesByProperties.push(BusinessCompanyObj);
+
+                });
+                console.log(employeesByProperties)
+            });
+        }
     }
 };
 
