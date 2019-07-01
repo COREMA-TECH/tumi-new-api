07d7ebfcbfd9f2c9804c7db57ea3b47f3f02643f
Olonyl Horacio Rocha Landeros <jose87.landeros@gmail.com>
2019-07-01 15:21:32 -0600

    fixed hour format and date in consolidated report punches

--- graphql/schemas/mutations/markedEmployeesMutation.js
+++ graphql/schemas/mutations/markedEmployeesMutation.js
@@ -2,6 +2,7 @@ import { inputInsertMarkedEmployees } from '../types/operations/insertTypes';
 import { inputUpdateMarkedEmployees } from '../types/operations/updateTypes';
 import { MarkedEmployeesType } from '../types/operations/outputTypes';
 import { GraphQLList, GraphQLInt, GraphQLBoolean } from 'graphql';
+import moment from 'moment';
 
 import Db from '../../models/models';
 
@@ -22,6 +23,30 @@ const MarkedEmployeesMutation = {
 
 		}
 	},
+	actualizarMarcasMasiva: {
+		type: new GraphQLList(MarkedEmployeesType),
+		description: 'Actualizar de forma masiva las marcadas',
+		resolve(source, args) {
+			return Db.models.MarkedEmployees.findAll().
+				then(_ => {
+					let marks = [];
+					_.map(_mark => {
+						let mark = _mark.dataValues;
+						let hours = mark.markedTime.split(' ');
+						let _hour = hours[0];
+						if (hours[1] == 'PM' && parseInt(hours[0]) != 12)
+							_hour = moment(_hour, "hh:mm").add(12, 'hours').format("HH:mm");
+						if (_hour.length == 4) _hour = `0${_hour}`;
+						marks.push({ id: mark.id, markedTime: _hour })
+					})
+					marks.map(_arr => {
+						return Db.models.MarkedEmployees.update({ markedTime: _arr.markedTime }, { where: { id: _arr.id } })
+					})
+
+				})
+		}
+	}
+	,
 	updateMarkedEmployees: {
 		type: MarkedEmployeesType,
 		description: 'Update Marked Employees Info',
